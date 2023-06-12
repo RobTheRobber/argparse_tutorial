75192e1357df000b8272493f180c136c3f899f41
Robert Auer <38085129+RobTheRobber@users.noreply.github.com>
2023-06-12 09:57:39 -0600

    added verbosity to prog.py

--- prog.py
+++ prog.py
@@ -1,11 +1,19 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+"""
+parser.add_argument("echo", help='the string you use here')
 args = parser.parse_args()
 print(args.echo)
 
 
-parser.add_argument("square", help="display a square of a given number")
+parser.add_argument("square", help="display a square of a given number", type= int)
 args = parser.parse_args()
 print(args.square**2)
+"""
+
+parser.add_argument("--verbosity", help="increase output verbosity")
+args = parser.parse_args()
+if args.verbosity:
+    print("verbosity turned on")
+
